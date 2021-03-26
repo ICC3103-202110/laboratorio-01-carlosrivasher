23ddca37cdb34a77ff8f7f1efcd64101c6740fd4
CarlosRivas <crivas1@miuandes.cl>
2021-03-26 10:56:57 -0300

    fourth commit

--- lab1.py
+++ lab1.py
@@ -1,26 +1,46 @@
 import random
 
 def matriz_cards(n):
-    cartas=[[],[]]
+    cards=[[],[]]
     for i in range(1, n+1):
-        cartas[0].append(i)
-        cartas[1].append(i)
-    random.shuffle(cartas[0])
-    random.shuffle(cartas[1])
-    return cartas
+        cards[0].append(i)
+        cards[1].append(i)
+    random.shuffle(cards[0])
+    random.shuffle(cards[1])
+    return cards
 
-n = int(input("¿Cuantos pares de carta desea jugar? (Con numeros por favor): "))
-p_j1 = 0
-p_j2 = 0
+def game_board(n):
+    board=[[],[]]
+    for i in range(1, n+1):
+        board[0].append("*")
+        board[1].append("*")
+    return board
+
+def board_print(a):
+    for i in range(len(a)):
+        print(a[i])
+
+def players(player):
+    if player==1:
+        player=2
+    else:
+        player=1
+    return player
+
+n = int(input("with how many cards do you want to play? (with number please): "))
+points1 = 0
+points2 = 0
+player=1
 
 cards = matriz_cards(n)
+board = game_board(n)
 
-for i in range(len(cards)):
-    print(cards[i])
+board_print(board)
+print("Player 1 begins")
 
-if p_j1 > p_j2:
-    print("PLAYER 1 WINS WITH", p_j1, "POINTS")
-elif p_j2 > p_j1:
-    print("PLAYER 2 WINS WITH", p_j2, "POINTS")
+if points1 > points2:
+    print("PLAYER 1 WINS WITH", points1, "POINTS")
+elif points2 > points1:
+    print("PLAYER 2 WINS WITH", points2, "POINTS")
 else:
-    print("DRAW WITH", p_j1, "POINTS")
+    print("DRAW WITH", points1, "POINTS")
