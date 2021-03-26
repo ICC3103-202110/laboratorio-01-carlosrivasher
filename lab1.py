f924760324d87cdd9ad4fb82b4562608a8adb2ae
CarlosRivas <crivas1@miuandes.cl>
2021-03-26 09:42:37 -0300

    third commit

--- lab1.py
+++ lab1.py
@@ -1,16 +1,26 @@
 import random
-n = int(input("with how many cards do you want to play?? (only numbers please): "))
 
+def matriz_cards(n):
+    cartas=[[],[]]
+    for i in range(1, n+1):
+        cartas[0].append(i)
+        cartas[1].append(i)
+    random.shuffle(cartas[0])
+    random.shuffle(cartas[1])
+    return cartas
+
+n = int(input("¿Cuantos pares de carta desea jugar? (Con numeros por favor): "))
 p_j1 = 0
 p_j2 = 0
 
-cartas = [[],[]]
-for i in range(1, n+1):
-    cartas[0].append(i)
-    cartas[1].append(i)
+cards = matriz_cards(n)
 
-random.shuffle(cartas[0])
-random.shuffle(cartas[1])
+for i in range(len(cards)):
+    print(cards[i])
 
-for i in range(len(cartas)):
-    print(cartas[i])
+if p_j1 > p_j2:
+    print("PLAYER 1 WINS WITH", p_j1, "POINTS")
+elif p_j2 > p_j1:
+    print("PLAYER 2 WINS WITH", p_j2, "POINTS")
+else:
+    print("DRAW WITH", p_j1, "POINTS")
